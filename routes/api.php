a8d48efe4b2098d7eb35289f784b1fd08f54ec65
Aravind Hota <aravind.hota@astech.ie>
2023-05-30 09:37:11 +0100

    Added Atuhentication to all users. performing tests.

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminAuthController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InstructorDetailsController;
 use App\Http\Controllers\LogbookController;
 use App\Http\Controllers\TraineeDetailsController;
 use App\Http\Controllers\TraineeLogbookController;
@@ -23,6 +24,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Admin
+
+Route::middleware('auth.admin')->group(function () {
+
 // Admin Login route
 Route::post('admin/login', [AdminAuthController::class, 'login']);
 
@@ -32,12 +37,31 @@ Route::post('admin/login/trainees', [AdminController::class, 'createtrainee']);
 
 //Routes for assigning logbooks
 Route::post('admin/login/logbooks', [LogbookController::class, 'assignLogbook']);
+});
 
-//Routes for instructor login
-Route::post('/instructors/login', [InstructorDetailsController::class, 'login']);
+//Trainees
 
+Route::middleware('auth.trainee')->group(function () {
 //Routes for Trainee login
 Route::post('/trainees/login', [TraineeDetailsController::class, 'login']);
 
 //Routes for accessing the logbooks by trainee
-Route::post('/tranee/login/{trianee_id}', [TraineeLogbookController::class, 'showLogbook']);
+Route::get('/trainee/login/{trainee_id}', [TraineeLogbookController::class, 'showLogbook']);
+
+//Route for logbook entry by trainee
+Route::post('/trainee/{trainee_id}', [TraineeLogbookController::class, 'storeLogbookEntry']);
+});
+
+//instructors
+
+Route::middleware('auth.instructor')->group(function () {
+
+//Routes for instructor login
+Route::post('/instructors/login', [InstructorDetailsController::class, 'login']);
+
+//Route for Instructor to view list of logbooks
+Route::get('/instructor/login/{instructor_id}/logbooks', [InstructorDetailsController::class, 'getLogbooks']);
+
+//Route for Instructor accessing logbooks
+Route::get('/instructor/login/{instructor_id}/logbooks/{logbookId}', [InstructorDetailsController::class, 'getTasks']);
+});
