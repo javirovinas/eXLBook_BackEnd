9b30c84fedb4021907413f9cef209176020f6aa6
Aravind Hota <aravind.hota@astech.ie>
2023-05-31 12:28:40 +0100

    removed authorizations

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -2,23 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Trainee_details;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use App\Models\Admin_login;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 class AdminAuthController extends Controller
 {
+    public function createAdmin(Request $request)
+    {
+        $data = $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $admin = Admin_login::create([
+            'username' => $data['username'],
+            'password' => $data['password'],
+            'api_token' => Str::random(60), // Generate a random token
+        ]);
+
+        return response()->json([
+            'message' => 'Admin created successfully',
+            'admin' => $admin,
+        ]);
+    }
+
     public function login(Request $request)
     {
-        $credentials = $request->only('username', 'password');
+        $credentials = $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
         $admin = Admin_login::where('username', $credentials['username'])->first();
 
-        if ($admin && $credentials['password'] === $admin->password) {
-            $admin->api_token = Str::random(60);
-            $admin->save();
-
-            return response()->json(['token' => $admin->api_token], 200);
+        if ($admin && $admin->password === $credentials['password']) {
+            $token = $admin->createToken('admin-token')->plainTextToken;
+            return response()->json(['token' => $token], 200);
         }
 
-        return response()->json(['error' => 'Invalid credentials'], 401);
+        throw ValidationException::withMessages(['username' => 'Invalid credentials']);
     }
 }
