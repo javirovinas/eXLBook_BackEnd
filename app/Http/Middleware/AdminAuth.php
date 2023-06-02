39452bb525da5896de432ab25a404b1b0aa23ff8
Aravind Hota <aravind.hota@astech.ie>
2023-06-02 10:57:07 +0100

    changes iun kernel

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -9,22 +9,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class AdminAuth
-{
-    public function handle($request, Closure $next)
-    {
-        $apiToken = $request->header('api_token');
-
-        if ($apiToken) {
-            // Check if the current user is an admin
-            $admin = Admin_login::where('api_token', $apiToken)->first();
-
-            if ($admin && Hash::check($apiToken, $admin->api_token)) {
-                // Admin is authenticated, proceed with the request
-                return $next($request);
-            }
-        }
-
-        // Admin authentication failed, return an unauthorized response
-        return response()->json(['error' => 'Unauthenticated'], 401);
-    }
+{//
 }
