7e960c6b51895cfb43cb5f67754f3da6da9073b6
Aravind Hota <aravind.hota@astech.ie>
2023-06-08 13:22:55 +0100

    authentication for admin done

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,15 +16,20 @@ class Kernel extends HttpKernel
      *
      * @var array<int, class-string|string>
      */
+
+    protected $routeMiddleware = [
+        'cors' => \App\Http\Middleware\CorsMiddleware::class,
+    ];
     protected $middleware = [
+        
         \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,     
-    
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+       // 'token.auth' => \App\Http\Middleware\TokenAuthMiddleware::class,
     ];
 
     /**
@@ -46,7 +51,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            //AdminAuth::class,
+            \App\Http\Middleware\CorsMiddleware::class,
         ],
     ];
 
