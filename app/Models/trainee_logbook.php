80ec17cfda0027fb84114c940e95b6b3bf1f98a0
rorymcc244 <133100443+rorymcc244@users.noreply.github.com>
2023-06-14 13:49:53 +0100

    Added date to logbooks

--- app/Models/trainee_logbook.php
+++ app/Models/trainee_logbook.php
@@ -2,14 +2,12 @@
 
 namespace App\Models;
 
-use App\Traits\Archivable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class trainee_logbook extends Model
 {
     use HasFactory;
-    //use Archivable;
 
 
     protected $table = 'tasks';
--- database/seeders/LogbookSeeder.php
+++ database/seeders/LogbookSeeder.php
@@ -15,14 +15,14 @@ class LogbookSeeder extends Seeder
     public function run(): void
     {
         $logbooks = [
-            ['logbook_name' => 'Maintenance_Logbook', 'trainee_id' => 4, 'instructor_id' => 3],
-            ['logbook_name' => 'Communications_Logbook', 'trainee_id' => 1, 'instructor_id' => 2],
-            ['logbook_name' => 'Physics_Logbook', 'trainee_id' => 3, 'instructor_id' => 1],
-            ['logbook_name' => 'Technologies_Logbook', 'trainee_id' => 2, 'instructor_id' => 4],
-            ['logbook_name' => 'Safety_Logbook', 'trainee_id' => 1, 'instructor_id' => 3],
-            ['logbook_name' => 'Navigation_Logbook', 'trainee_id' => 4, 'instructor_id' => 1],
-            ['logbook_name' => 'Medical_Logbook', 'trainee_id' => 3, 'instructor_id' => 2], 
-            ['logbook_name' => 'Research_Logbook', 'trainee_id' => 4, 'instructor_id' => 4]
+            ['logbook_name' => 'Maintenance_Logbook', 'date' => '2023-02-23', 'trainee_id' => 4, 'instructor_id' => 3],
+            ['logbook_name' => 'Communications_Logbook', 'date' => '2023-02-20', 'trainee_id' => 1, 'instructor_id' => 2],
+            ['logbook_name' => 'Physics_Logbook', 'date' => '2023-02-02', 'trainee_id' => 3, 'instructor_id' => 1],
+            ['logbook_name' => 'Technologies_Logbook', 'date' => '2023-01-28', 'trainee_id' => 2, 'instructor_id' => 4],
+            ['logbook_name' => 'Safety_Logbook', 'date' => '2023-03-01', 'trainee_id' => 1, 'instructor_id' => 3],
+            ['logbook_name' => 'Navigation_Logbook', 'date' => '2023-02-11', 'trainee_id' => 4, 'instructor_id' => 1],
+            ['logbook_name' => 'Medical_Logbook', 'date' => '2023-01-18', 'trainee_id' => 3, 'instructor_id' => 2], 
+            ['logbook_name' => 'Research_Logbook', 'date' => '2023-03-04', 'trainee_id' => 4, 'instructor_id' => 4]
         ];
 
         foreach ($logbooks as $logbook) {
