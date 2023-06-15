f7948d29920555d4d22162222f42ad6e2b400c02
rorymcc244 <133100443+rorymcc244@users.noreply.github.com>
2023-06-15 09:26:54 +0100

    Task Seeder updates

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -20,21 +20,21 @@ class TaskSeeder extends Seeder
             ["trainee_id" => 4, "logbook_id" => 2, "work_order_no" => 637951, "task_detail" => "Tested aeronautical radio systems", "category" => "A1", "ATA" => "13"],
             ["trainee_id" => 4, "logbook_id" => 2, "work_order_no" => 637952, "task_detail" => "Fixed VHF radios", "category" => "A1", "ATA" => "13",],
             ["trainee_id" => 4, "logbook_id" => 2, "work_order_no" => 637953, "task_detail" => "Evaluated range performance of communication technologies", "category" => "A1", "ATA" => "13"],
-            ["trainee_id" => 1, "logbook_id" => 3, "work_order_no" => 712008, "task_detail" => "Calculated aerodynamics questions", "category" => "C2", "ATA" => "07"],
-            ["trainee_id" => 1, "logbook_id" => 3, "work_order_no" => 712009, "task_detail" => "Experimented with different aircraft lifts", "category" => "C2", "ATA" => "07"],
-            ["trainee_id" => 1, "logbook_id" => 3, "work_order_no" => 712010, "task_detail" => "Studied how drag affects flight", "category" => "C2", "ATA" => "07"],
-            ["trainee_id" => 1, "logbook_id" => 3, "work_order_no" => 712011, "task_detail" => "Explored physics concepts involved in aviation", "category" => "C2", "ATA" => "07"],
+            ["trainee_id" => 1, "logbook_id" => 3, "work_order_no" => 712008, "task_detail" => "Calculated aerodynamics questions", "category" => "C2", "ATA" => "7"],
+            ["trainee_id" => 1, "logbook_id" => 3, "work_order_no" => 712009, "task_detail" => "Experimented with different aircraft lifts", "category" => "C2", "ATA" => "7"],
+            ["trainee_id" => 1, "logbook_id" => 3, "work_order_no" => 712010, "task_detail" => "Studied how drag affects flight", "category" => "C2", "ATA" => "7"],
+            ["trainee_id" => 1, "logbook_id" => 3, "work_order_no" => 712011, "task_detail" => "Explored physics concepts involved in aviation", "category" => "C2", "ATA" => "7"],
             ["trainee_id" => 3, "logbook_id" => 4, "work_order_no" => 854209, "task_detail" => "Fixed some in-flight connectivity issues", "category" => "A", "ATA" => "19"],
-            ["trainee_id" => 2, "logbook_id" => 1, "work_order_no" => 500238, "task_detail" => "Performed engine inspections", "category" => "B", "ATA" => "71"],
-            ["trainee_id" => 2, "logbook_id" => 1, "work_order_no" => 500240, "task_detail" => "Conducted aircraft fueling operations", "category" => "B", "ATA" => "73"],
-            ["trainee_id" => 4, "logbook_id" => 2, "work_order_no" => 637955, "task_detail" => "Inspected aircraft landing gear", "category" => "A1", "ATA" => "32"],
+            ["trainee_id" => 2, "logbook_id" => 1, "work_order_no" => 500238, "task_detail" => "Performed engine inspections", "category" => "B", "ATA" => "11"],
+            ["trainee_id" => 2, "logbook_id" => 1, "work_order_no" => 500240, "task_detail" => "Conducted aircraft fueling operations", "category" => "B", "ATA" => "3"],
+            ["trainee_id" => 4, "logbook_id" => 2, "work_order_no" => 637955, "task_detail" => "Inspected aircraft landing gear", "category" => "A1", "ATA" => "2"],
             ["trainee_id" => 4, "logbook_id" => 2, "work_order_no" => 637956, "task_detail" => "Tested aircraft hydraulic systems", "category" => "A1", "ATA" => "29"],
             ["trainee_id" => 4, "logbook_id" => 2, "work_order_no" => 637958, "task_detail" => "Checked aircraft navigation instruments", "category" => "A1", "ATA" => "22"],
-            ["trainee_id" => 1, "logbook_id" => 3, "work_order_no" => 712013, "task_detail" => "Practiced aircraft emergency procedures", "category" => "C2", "ATA" => "76"],
-            ["trainee_id" => 1, "logbook_id" => 3, "work_order_no" => 712015, "task_detail" => "Performed weight and balance calculations", "category" => "C2", "ATA" => "75"],
-            ["trainee_id" => 1, "logbook_id" => 3, "work_order_no" => 712017, "task_detail" => "Conducted flight simulation sessions", "category" => "C2", "ATA" => "78"],
-            ["trainee_id" => 3, "logbook_id" => 4, "work_order_no" => 854213, "task_detail" => "Inspected aircraft avionics systems", "category" => "A", "ATA" => "34"],
-            ["trainee_id" => 3, "logbook_id" => 4, "work_order_no" => 854215, "task_detail" => "Performed engine run-up tests", "category" => "A", "ATA" => "72"]
+            ["trainee_id" => 1, "logbook_id" => 3, "work_order_no" => 712013, "task_detail" => "Practiced aircraft emergency procedures", "category" => "C2", "ATA" => "12"],
+            ["trainee_id" => 1, "logbook_id" => 3, "work_order_no" => 712015, "task_detail" => "Performed weight and balance calculations", "category" => "C2", "ATA" => "5"],
+            ["trainee_id" => 1, "logbook_id" => 3, "work_order_no" => 712017, "task_detail" => "Conducted flight simulation sessions", "category" => "C2", "ATA" => "8"],
+            ["trainee_id" => 3, "logbook_id" => 4, "work_order_no" => 854213, "task_detail" => "Inspected aircraft avionics systems", "category" => "A", "ATA" => "4"],
+            ["trainee_id" => 3, "logbook_id" => 4, "work_order_no" => 854215, "task_detail" => "Performed engine run-up tests", "category" => "A", "ATA" => "7"]
         ];
 
         foreach ($tasks as $task) {
